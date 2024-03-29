6417bfff57f82bb9aa9900098e9d275bc0cc0d50
Kat Kipling <kat.kipling@proton.me>
2024-03-29 22:58:08 +0000

    Basic OpenGL Setup

--- OpenGL Test Proj/Program.cs
+++ OpenGL Test Proj/Program.cs
@@ -1,8 +1,26 @@
-﻿
-public class Program
+﻿using System;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace OpenGlTesting
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {
+            // Default settings
+            GameWindowSettings gws = GameWindowSettings.Default;
+            NativeWindowSettings nws = NativeWindowSettings.Default;
+
+            // Changing properties as needed
+            nws.APIVersion = Version.Parse("4.1"); // 4.1 as aimed for recent hardware on any platform.
+            nws.ClientSize = new Vector2i(1920, 1080);
+            nws.Title = "Testing OpenGL";
+
+            // Running window
+            GameWindow MainWindow = new GameWindow(gws, nws);
+
+            MainWindow.Run();
+        }
     }
 }
