68bac20ebe2fab5f8f20a047a08bcd460a0bcbf7
Kat Kipling <kat.kipling@proton.me>
2024-03-29 23:07:18 +0000

    Window update on every frame

--- OpenGL Test Proj/Program.cs
+++ OpenGL Test Proj/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace OpenGlTesting
 {
@@ -12,13 +14,24 @@ namespace OpenGlTesting
             GameWindowSettings gws = GameWindowSettings.Default;
             NativeWindowSettings nws = NativeWindowSettings.Default;
 
-            // Changing properties as needed
+            // Changing properties as needed for Game Window
+            gws.UpdateFrequency = 144;
+
+            // Changing properties as needed for Native Window
             nws.APIVersion = Version.Parse("4.1"); // 4.1 as aimed for recent hardware on any platform.
             nws.ClientSize = new Vector2i(1920, 1080);
             nws.Title = "Testing OpenGL";
 
             // Running window
             GameWindow MainWindow = new GameWindow(gws, nws);
+
+            int i = 0;
+            // Runs every frame
+            MainWindow.UpdateFrame += (FrameEventArgs args)  =>
+            {
+                Console.WriteLine($"{i}");
+                i++;
+            };
 
             MainWindow.Run();
         }
