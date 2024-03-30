113248fdd44df939763c1a2e399260c61fb49447
Kat Kipling <kat.kipling@proton.me>
2024-03-30 05:55:51 +0000

    Added blank methods

--- OpenGL Test Proj/Camera.cs
+++ OpenGL Test Proj/Camera.cs
@@ -1,7 +1,36 @@
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
 namespace OpenGlTesting
 {
     internal class Camera
     {
-        
+        public Camera()
+        {
+            
+        }
+
+        public Matrix4 GetProjectionMatrix()
+        {
+
+        }
+
+        private void UpdateVectors()
+        {
+
+        }
+
+        public void InputController(KeyboardState keybIn, MouseState mouseIn, FrameEventArgs eventArgs)
+        {
+
+        }
+
+        public void Update(KeyboardState keybIn, MouseState mouseIn, FrameEventArgs eventArgs)
+        {
+            InputController(keybIn, mouseIn, eventArgs);
+        }
+
+
     }
 }
