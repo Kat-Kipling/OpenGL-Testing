a255bbccfb4b7623cec5e7d9930994f2e578f113
Kat Kipling <kat.kipling@proton.me>
2024-03-30 03:02:13 +0000

    IBO complete

--- OpenGL Test Proj/Render/IBO.cs
+++ OpenGL Test Proj/Render/IBO.cs
@@ -5,6 +5,28 @@ namespace OpenGlTesting
 {
     internal class IBO
     {
+        public int Id;
+        
+        public IBO(List<uint> data)
+        {
+            Id = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Id);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, data.Count * sizeof(uint), data.ToArray(), BufferUsageHint.StaticDraw);
+        }
 
+        public void Bind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Id);
+        }
+
+        public void Unbind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+        }
+
+        public void Delete()
+        {
+            GL.DeleteBuffer(Id);
+        }
     }
 }
