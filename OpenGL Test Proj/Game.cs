ba65c116ae0b6ed4dd30059c1557e3c65bce8c7d
Kat Kipling <kat.kipling@proton.me>
2024-03-30 06:53:19 +0000

    Triangle to cube

--- OpenGL Test Proj/Game.cs
+++ OpenGL Test Proj/Game.cs
@@ -18,16 +18,57 @@ namespace OpenGlTesting
         float yRot = 0;
         List<Vector3> vertices = new List<Vector3>
         {
-            new Vector3(-0.5f, 0.5f, 0f),
-            new Vector3(0.5f, 0.5f, 0f),
-            new Vector3(0.5f, -0.5f, 0f),
-            //new Vector3(-0.5f, -0.5f, 0f)
+            // front face
+            new Vector3(-0.5f, 0.5f, 0.5f), // topleft vert
+            new Vector3(0.5f, 0.5f, 0.5f), // topright vert
+            new Vector3(0.5f, -0.5f, 0.5f), // bottomright vert
+            new Vector3(-0.5f, -0.5f, 0.5f), // bottomleft vert
+            // right face
+            new Vector3(0.5f, 0.5f, 0.5f), // topleft vert
+            new Vector3(0.5f, 0.5f, -0.5f), // topright vert
+            new Vector3(0.5f, -0.5f, -0.5f), // bottomright vert
+            new Vector3(0.5f, -0.5f, 0.5f), // bottomleft vert
+            // back face
+            new Vector3(0.5f, 0.5f, -0.5f), // topleft vert
+            new Vector3(-0.5f, 0.5f, -0.5f), // topright vert
+            new Vector3(-0.5f, -0.5f, -0.5f), // bottomright vert
+            new Vector3(0.5f, -0.5f, -0.5f), // bottomleft vert
+            // left face
+            new Vector3(-0.5f, 0.5f, -0.5f), // topleft vert
+            new Vector3(-0.5f, 0.5f, 0.5f), // topright vert
+            new Vector3(-0.5f, -0.5f, 0.5f), // bottomright vert
+            new Vector3(-0.5f, -0.5f, -0.5f), // bottomleft vert
+            // top face
+            new Vector3(-0.5f, 0.5f, -0.5f), // topleft vert
+            new Vector3(0.5f, 0.5f, -0.5f), // topright vert
+            new Vector3(0.5f, 0.5f, 0.5f), // bottomright vert
+            new Vector3(-0.5f, 0.5f, 0.5f), // bottomleft vert
+            // bottom face
+            new Vector3(-0.5f, -0.5f, 0.5f), // topleft vert
+            new Vector3(0.5f, -0.5f, 0.5f), // topright vert
+            new Vector3(0.5f, -0.5f, -0.5f), // bottomright vert
+            new Vector3(-0.5f, -0.5f, -0.5f), // bottomleft vert
         };
 
         List<uint> indices = new List<uint>
         {
-            0, 1, 2,
-            //2, 3, 0
+            0,1,2,
+            2,3,0,
+
+            4,5,6,
+            6,7,4,
+
+            8,9,10,
+            10,11,8,
+
+            12,13,14,
+            14,15,12,
+
+            16,17,18,
+            18,19,16,
+
+            20,21,22,
+            22,23,20
         };
 
         public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
