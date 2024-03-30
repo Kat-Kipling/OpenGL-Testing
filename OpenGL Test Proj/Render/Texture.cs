db7c362194c01055fe58f30f49d4c5939cfbc3a1
Kat Kipling <kat.kipling@proton.me>
2024-03-30 07:13:27 +0000

    Completed texture class

--- OpenGL Test Proj/Render/Texture.cs
+++ OpenGL Test Proj/Render/Texture.cs
@@ -1,9 +1,45 @@
 using OpenTK.Graphics.OpenGL4;
+using StbImageSharp;
 
 namespace OpenGlTesting
 {
-    internal class Texture
+    internal class Texture : IDisposable
     {
-        public int Id;
+        public int Id {get;}
+
+        public Texture(string filename)
+        {
+            Id = GL.GenTexture();
+
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, Id);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            StbImage.stbi_set_flip_vertically_on_load(1);
+            ImageResult texture = ImageResult.FromStream(File.OpenRead("Resources/" + filename), ColorComponents.RedGreenBlueAlpha);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, texture.Width, texture.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, texture.Data);
+        
+            Unbind();
+        }
+
+        public void Bind()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, Id);
+        }
+
+        public void Unbind()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteTexture(Id);
+        }
     }
 }
