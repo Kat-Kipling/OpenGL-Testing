89f726fea0b6cfd881c4cc841e66f7237fc007e3
Kat Kipling <kat.kipling@proton.me>
2024-03-30 02:49:06 +0000

    Basic game methods implemented

--- OpenGL Test Proj/Game.cs
+++ OpenGL Test Proj/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using OpenTK.Graphics.ES11;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -8,13 +9,51 @@ namespace OpenGlTesting
 {
     internal class Game : GameWindow
     {
-        private static int SCREENWIDTH;
-        private static int SCREENHEIGHT;
+        int ScreenWidth;
+        int ScreenHeight;
         public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
             this.CenterWindow(new Vector2i(width, height));
-            SCREENWIDTH = width;
-            SCREENHEIGHT = height;
+            ScreenWidth = width;
+            ScreenHeight = height;
+        }
+
+        // Called on launch
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        // On window size change
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+            this.ScreenWidth = e.Width;
+            this.ScreenHeight = e.Height;
+        }
+
+        // On frame rendered - for shaders etc
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(1.0f, 0.4f, 0.7f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            Context.SwapBuffers(); // Swap draw window to display window
+
+            base.OnRenderFrame(args);
+        }
+
+        // On frame update - logic for game
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        // Called on close
+        protected override void OnUnload()
+        {
+            base.OnUnload();
         }
     }
 }
