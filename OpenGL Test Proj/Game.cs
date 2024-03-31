0ccf1e98c1728ad90325372c4aca1e7400e37cc8
Kat Kipling <kat.kipling@proton.me>
2024-03-31 17:50:09 +0000

    Added basic model loading from file

--- OpenGL Test Proj/Game.cs
+++ OpenGL Test Proj/Game.cs
@@ -14,60 +14,6 @@ namespace OpenGlTesting
         Camera Camera;
         ShaderProgram Shaders;
         float yRot = 0.0f;
-        List<Vector3> vertices = new List<Vector3>
-        {
-            // front face
-            new Vector3(-0.5f, 0.5f, 0.5f), // topleft vert
-            new Vector3(0.5f, 0.5f, 0.5f), // topright vert
-            new Vector3(0.5f, -0.5f, 0.5f), // bottomright vert
-            new Vector3(-0.5f, -0.5f, 0.5f), // bottomleft vert
-            // right face
-            new Vector3(0.5f, 0.5f, 0.5f), // topleft vert
-            new Vector3(0.5f, 0.5f, -0.5f), // topright vert
-            new Vector3(0.5f, -0.5f, -0.5f), // bottomright vert
-            new Vector3(0.5f, -0.5f, 0.5f), // bottomleft vert
-            // back face
-            new Vector3(0.5f, 0.5f, -0.5f), // topleft vert
-            new Vector3(-0.5f, 0.5f, -0.5f), // topright vert
-            new Vector3(-0.5f, -0.5f, -0.5f), // bottomright vert
-            new Vector3(0.5f, -0.5f, -0.5f), // bottomleft vert
-            // left face
-            new Vector3(-0.5f, 0.5f, -0.5f), // topleft vert
-            new Vector3(-0.5f, 0.5f, 0.5f), // topright vert
-            new Vector3(-0.5f, -0.5f, 0.5f), // bottomright vert
-            new Vector3(-0.5f, -0.5f, -0.5f), // bottomleft vert
-            // top face
-            new Vector3(-0.5f, 0.5f, -0.5f), // topleft vert
-            new Vector3(0.5f, 0.5f, -0.5f), // topright vert
-            new Vector3(0.5f, 0.5f, 0.5f), // bottomright vert
-            new Vector3(-0.5f, 0.5f, 0.5f), // bottomleft vert
-            // bottom face
-            new Vector3(-0.5f, -0.5f, 0.5f), // topleft vert
-            new Vector3(0.5f, -0.5f, 0.5f), // topright vert
-            new Vector3(0.5f, -0.5f, -0.5f), // bottomright vert
-            new Vector3(-0.5f, -0.5f, -0.5f), // bottomleft vert
-        };
-
-        List<uint> indices = new List<uint>
-        {
-            0,1,2,
-            2,3,0,
-
-            4,5,6,
-            6,7,4,
-
-            8,9,10,
-            10,11,8,
-
-            12,13,14,
-            14,15,12,
-
-            16,17,18,
-            18,19,16,
-
-            20,21,22,
-            22,23,20
-        };
 
         EntityLoader loader = new EntityLoader();
         Renderer renderer = new Renderer();
@@ -84,7 +30,7 @@ namespace OpenGlTesting
         {
             base.OnLoad();
 
-            cube = loader.LoadToVao(vertices, indices);
+            cube = ModelLoader.LoadModelFromObj("pp", loader);
             Shaders = new ShaderProgram("Shaders/Default.vert", "Shaders/Default.frag");
 
             Camera = new Camera(ScreenWidth, ScreenHeight, Vector3.Zero);
--- OpenGL Test Proj/Render/ModelLoader.cs
+++ OpenGL Test Proj/Render/ModelLoader.cs
@@ -1,10 +1,80 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
 namespace OpenGlTesting
 {
     internal class ModelLoader
     {
-        /* public static Model loadModelFromObj(string filename)
+        public static Model LoadModelFromObj(string filename, EntityLoader loader)
         {
+            string? line;
+            List<Vector3> vertices = new List<Vector3>();
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector2> textCoords = new List<Vector2>();
+            List<uint> indices = new List<uint>();
             
-        } */
+            try
+            {
+                StreamReader file = new StreamReader("Resources/" + filename + ".obj");
+                List<Vector3> tempNormals = new List<Vector3>();
+                List<Vector2> tempTexCords = new List<Vector2>();
+
+                line = file.ReadLine();
+
+                while(line != null)
+                {
+                    string[] lineParts = line.Split(' '); // First item will be the key (i.e. v, vt, f etc), further items will be the values for that data
+
+                    if(line.StartsWith("v ")) // v = vertex position
+                    {
+                        Vector3 vertix = new Vector3(float.Parse(lineParts[1]), float.Parse(lineParts[2]), float.Parse(lineParts[3]));
+                        vertices.Add(vertix);
+                    }
+                    else if(line.StartsWith("vt ")) // vt = texture coords
+                    {
+                        Vector2 texCoord = new Vector2(float.Parse(lineParts[1]), float.Parse(lineParts[2]));
+                        tempTexCords.Add(texCoord);
+                    }
+                    else if(line.StartsWith("vt ")) // vn = normals
+                    {
+                        Vector3 normal = new Vector3(float.Parse(lineParts[1]), float.Parse(lineParts[2]), float.Parse(lineParts[3]));
+                        tempNormals.Add(normal);
+
+                    }
+                    else if(line.StartsWith("f ")) // f = face
+                    {
+                        /*
+                        .obj files have a section for every triangle (face) in a model.
+                        This section is split into 3 sets of 3 values, seperated by a forward slash (/)
+                        Every set of values (i.e., 20/10/40) stores
+                            - The index of the vertex position for this vertex (20th vertex position)
+                            - The index for texture coordinate (10th texture coordinate)
+                            - The index for the normal (40th normal)
+                        By storing 3 of these sets, we represent a single triangle.
+                        Indexed from 1, while lists are indexed from 0. So we have to -1 to these values for our case.
+                        */
+                        for(int i = 1; i <= 3; i++)
+                        {
+                            string[] vertexData = lineParts[i].Split("/");
+                            uint vertexPos = uint.Parse(vertexData[0]) - 1;
+                            int texPos = int.Parse(vertexData[1]) - 1;
+                            int normalPos = int.Parse(vertexData[2]) - 1;
+
+                            indices.Add(vertexPos);
+                            textCoords.Add(tempTexCords[texPos]);
+                            normals.Add(tempNormals[normalPos]);
+                        }
+                    }
+                    line = file.ReadLine();
+                }
+
+                file.Close();
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("File not found. Please check file name exists.\n" + e);
+            }
+            return loader.LoadToVao(vertices, indices);
+        }
     }
 }
